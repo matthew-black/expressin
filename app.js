7be8af1cc642d88a91382ca90f1db465c17c2591
Matthew Black <matthewallenblack@gmail.com>
2018-01-20 15:08:58 -0600

    Create '/' GET route. This is sorta neat...

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 var app = express();
 
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
 app.listen(3000, function() {
   console.log("Matthew's first Express server has started!")
 });
