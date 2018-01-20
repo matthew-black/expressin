22099c1d52dab630e8621c5bb6a92dc8f83170ba
Matthew Black <matthewallenblack@gmail.com>
2018-01-20 15:22:06 -0600

    Create public folder and configure its path.

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const path = require('path');
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+// <middleware example>
+  // var logger = function(req, res, next){
+  //   console.log('Logging...');
+  //   next();
+  // }
+  // app.use(logger);
+// </middleware example>
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Sets Path to Public Folder, or Static Folder (CSS files, jQuery, etc...)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.send('Hello Derper!');
 });
 
-app.listen(3000, function() {
+app.listen(3000, function(){
   console.log("Matthew's first Express server has started!")
 });
+
